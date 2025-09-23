f38f4dd17ec0cbfe7940491d57d151e4a33c67e8
Dhomini Silva <dhominisilva1@gmail.com>
2025-09-23 19:34:18 -0300

    Atualizando a lista para pesquisar todos do bd e que esteja ativo, add uma coluna de ativo no bd e criei um endpoint para deixar um usuario paciente inativo

--- src/main/java/med/voll/api/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/medico/MedicoRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Page<Medico> findAllByAtivoTrue(Pageable paginacao);
--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -26,12 +26,15 @@ public class Paciente {
     @Embedded
     private Endereco endereco;
 
+    private Boolean ativo;
+
     public Paciente(DadosCadastroPaciente dados) {
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
         this.cpf = dados.cpf();
         this.endereco = new Endereco(dados.endereco());
+        this.ativo = true;
     }
 
     public void atualizarInformacoes(@Valid DadosAtualizacaoPaciente dados) {
@@ -44,5 +47,10 @@ public class Paciente {
         if (dados.endereco() != null) {
             this.endereco.atualizarInformacoes((dados.endereco()));
         }
+    }
+
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
